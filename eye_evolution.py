b8976ba379a05883fd47a038072bef2e72a86ed7
Alix Gonnot <alix.gonnot@gmail.com>
2017-02-19 23:36:44 +0100

    Correction bugs

--- eye_evolution.py
+++ eye_evolution.py
@@ -4,7 +4,7 @@ import operator
 import random
 import csv
 
-#defintion de ce qu'est un individu
+#definition de ce qu'est un individu
 class Individual:
 
     def __init__(self, rc, t, ang, ref):
@@ -18,20 +18,79 @@ class Individual:
         #return "Individu: rco({}), ti({}), at({}), n0({})".format(self.rco, self.ti, self.at, self.n0)
         return "Individu: v({})".format(self.fit)
 
+    def mutate(self) :
+        rd_rco1 = random.randint(1, 5)
+        rd_rco2 = random.randint(1, 2)
+        if rd_rco1 == 1 :
+            if rd_rco2 == 1 :
+                self.rco = self.rco + pas_rco
+                if self.rco > 10000 :
+                    self.rco = 10000.0
+            else :
+                self.rco = self.rco - pas_rco
+                if self.rco < w/2 :
+                    self.rco = w/2
+
+        rd_ti1 = random.randint(1, 5)
+        rd_ti2 = random.randint(1, 2)
+        if rd_ti1 == 1 :
+            if rd_ti2 == 1 :
+                self.ti = self.ti + pas_ti
+                if (self.ti) >= w/2 :
+                    self.ti = (w/2) - pas_ti
+            else :
+                self.ti = self.ti - pas_ti
+                if (self.ti) < 0 :
+                    self.ti = 0
+
+        rd_at1 = random.randint(1, 5)
+        rd_at2 = random.randint(1, 2)
+        if rd_at1 == 1 :
+            if rd_at2 == 1 :
+                self.at = self.at + pas_at
+                if (self.at) >= (math.pi/2) :
+                    self.at=(math.pi/2)-pas_at
+            else :
+                self.at = self.at - pas_at
+                if (self.at) < 0 :
+                    self.at = 0.0
+
+        rd_n01 = random.randint(1, 5)
+        rd_n02 = random.randint(1, 2)
+        if rd_n01 == 1 :
+            if rd_n02 == 1 :
+                self.n0 = self.n0 + pas_n0
+                if (self.n0) > 1.55 :
+                    self.n0 = 1.55
+            else :
+                self.n0 = self.n0 - pas_n0
+                if (self.n0) < 1.35 :
+                    self.n0 = 1.35
+
+    def isFit(self, rounded_n0, a, p, r1):
+        return not (((abs(self.at)>tshld and abs(self.rco-w/2.0)>tshld)
+        or (abs(self.at)>tshld and self.ti > w*math.cos(self.at)/2.0)
+        or (abs(rounded_n0-1.35)<0.0005 and abs(self.at)<tshld and self.ti>(0.5*(w-sq)))
+        or (abs(rounded_n0-1.35)<0.0005 and abs(self.at)>tshld and self.ti>(0.5*(w*math.cos(self.at)-sq)))
+        or (abs(rounded_n0-1.35)>0.0005 and ((p > (r1*a)/2.0) or (p < a/2.0)))))
+
 ####################################################################################################################################
 
 def tri(indivs) :
     print("tri")
 
+#selection aleatoire d'un individu
 def select_parent(fit_indvs, p_rep):
     rd1 = random.random()
+    #print(rd1)
     sum_p = 0;
     for i in range(0, len(fit_indvs)) :
         sum_p = sum_p + p_rep[i]
         if (rd1 < sum_p) :
-            return fit_indvs[i]
-    return None
+            break
+    return fit_indvs[i]
 
+#recomposition discrete des genes des parents
 def create_child(parent1, parent2) :
     rd1 = random.randint(1,2)
     if(rd1 == 1) :
@@ -59,117 +118,31 @@ def create_child(parent1, parent2) :
 
     return Individual(rc, t, ang, ref)
 
-def mutateV2(child):
-    mutationChance = 0.3
-    if random.random() > mutationChance:
-        return
-    rd1 = random.randint(1, 4)
-
-    if rd1 == 1:
-        child.rco = random.uniform(w/2,10000)
-    elif rd1 == 2:
-        child.ti = random.uniform(0,w/2)
-    elif rd1  == 3:
-        child.at = random.uniform(0,math.pi/2)
-    else:
-        child.n0 = random.uniform(1.35,1.55)
-
-def mutateV3(child):
-    rd1 = random.randint(1, 2)
-    if rd1 == 1:
-        child.rco = random.uniform(w/2,10000)
-    rd2 = random.randint(1, 4)
-    if rd2 == 1:
-        child.ti = random.uniform(0,w/2)
-    rd3 = random.randint(1, 4)
-    if rd3  == 1:
-        child.at = random.uniform(0,math.pi/2)
-    rd4 = random.randint(1, 4)
-    if rd4 == 1:
-        child.n0 = random.uniform(1.35,1.55)
-
-def mutate(child) :
-    #doit-on vérifier que les valeurs sont dans les intervales prévu -> oui !
-    #penser à changer les intervalles !!!
-    rd1 = random.randint(1, 4)
-    rd2 = random.randint(1, 2)
-    if rd1 == 1 :
-        pas = 9.99
-        if rd2 == 1 :
-            child.rco = child.rco + pas
-            if child.rco > 10000 :
-                child.rco = 10000
-        else :
-            child.rco = child.rco - pas
-            if child.rco < w/2 :
-                child.rco = w/2
-
-    rd1 = random.randint(1, 4)
-    rd2 = random.randint(1, 2)
-    if rd1 == 1 :
-        pas = 0.00075
-        if rd2 == 1 :
-            child.ti = child.ti + pas
-            if (child.ti) >= w/2 :
-                child.ti = (w/2) - pas
-        else :
-            child.ti = child.ti - pas
-            if (child.ti) < 0 :
-                child.ti = 0
-
-    rd1 = random.randint(1, 4)
-    rd2 = random.randint(1, 2)
-    if rd1 == 1 :
-        pas = 0.00157
-        if rd2 == 1 :
-            child.at = child.at + pas
-            if (child.at) >= (math.pi/2) :
-                child.at=(math.pi/2)-pas
-        else :
-            child.at = child.at - pas
-            if (child.at) < 0 :
-                child.at = 0
-
-    rd1 = random.randint(1, 4)
-    rd2 = random.randint(1, 2)
-    if rd1 == 1 :
-        pas = 0.0002
-        if rd2 == 1 :
-            child.n0 = child.n0 + pas
-            if (child.n0) > 1.55 :
-                child.n0 = 1.55
-        else :
-            child.n0 = child.n0 - pas
-            if (child.n0) < 1.35 :
-                child.n0 = 1.35
-
-def myEq(v1,v2,s):
-    if v1 != 0:
-        return abs((v1-v2)/v1) < s
-    else:
-        return abs(v2) < s
-
-def myDiff(v1,v2,s):
-    if v1 != 0:
-        return abs((v1-v2)/v1) > s
-    else:
-        return abs(v2) > s
-
 ############################################################################################################################
-dataFile = open("data.csv","w")
-dataFile.write("g,rco,ti,at,n0,p,a,r1,theta,v\n")
+#ouverture du fichier pour inscription des résultats
+dataFile = open("data11.csv","w")
+dataFile.write("g,type,rco,ti,at,n0,p,a,r1,theta,v\n")
 
-#paramètres de la simulation
+#paramètres de l'algorithme
 w = 1.5
 light = math.exp(6)
-nb_indivs = 50
-nb_iteration = 5000
+nb_indivs = 100
+nb_iteration = 10000
 tshld = 0.00001
 t_n0 = 0.001
 tshld_rco = tshld * 10000
 sq = math.sqrt(math.exp(1)/(0.746*math.sqrt(light)))
 
+pas_rco = (10000 - (w/2.0))/1000.0
+pas_ti = (w/2.0)/1000
+pas_at = (math.pi/2.0)/1000
+pas_n0 = 0.001 # pas de n0 fixé manuellement car on lit dans le fichier directement
 #lecture du fichier indice_refraction
+
+random.seed(11)
+
+print("pas_rco", pas_rco, "pas_at", pas_at, "pas_ti", pas_ti, "pas_n0", pas_n0)
+
 fichier = open("indice_refraction_facile.dat", "r")
 
 dic_indice = {}
@@ -182,111 +155,129 @@ fichier.close()
 #création de la population intiale
 indivs = []
 for i in range (0, nb_indivs) :
-    indivs.append(Individual(10000, 0.0, 0.0, 1.35))
+    indivs.append(Individual(10000.0, 0.0, 0.0, 1.35))
 
 #pour toutes les itérations
 for it in range(0, nb_iteration) :
     #test de la fitness d'une population
     fit_indvs = []
+    vmax = -1
+    sum_rco = 0
+    sum_ti = 0
+    sum_at = 0
+    sum_n0 = 0
+    sum_p = 0
+    sum_a = 0
+    sum_r1 = 0
+    sum_theta = 0
+    sum_v = 0
     for ind in range (0, nb_indivs) :
-        if ((abs(indivs[ind].at)>tshld and abs(indivs[ind].rco-w/2)>tshld)
-        or (abs(indivs[ind].at)>tshld and indivs[ind].ti > w*math.cos(indivs[ind].at)/2)
-        or (abs(indivs[ind].n0-1.35)<t_n0 and abs(indivs[ind].at)<tshld and indivs[ind].ti>((1/2)*(w-sq)))
-        or (abs(indivs[ind].n0-1.35)<t_n0 and abs(indivs[ind].at)>tshld and indivs[ind].ti>((1/2)*(w*math.cos(indivs[ind].at)-sq)))
-        or (abs(indivs[ind].n0-1.35)>t_n0 and ((p > (r1*a)/2) or (p < a/2)))) :
-            #print ("individu non valide")
-            pass
-        else :
-            #calcul des grandeurs p et a
-            # print("rco",indivs[ind].rco)
+        # print("n0", indivs[ind].n0)
+        if abs(indivs[ind].rco-w/2.0) < tshld: #version 1
+            p = (w/2.0)*(1.0 + math.sin(indivs[ind].at))
+            a = w*math.cos(indivs[ind].at)-2.0*indivs[ind].ti
+        elif indivs[ind].rco > w/2.0: #version 2
+            p = indivs[ind].rco - (math.sqrt(math.pow(indivs[ind].rco,2.0) - (math.pow(w,2.0)/4.0)))
+            a = w - 2.0*indivs[ind].ti
+        else:
+            print("Erreur -> rc0 < w/2")
 
-            # print("n0", indivs[ind].n0)
-            if abs(indivs[ind].rco-w/2) < tshld: #version 1
-                p = (w/2)*(1 + math.sin(indivs[ind].at))
-                a = w*math.cos(indivs[ind].at)-2*indivs[ind].ti
-            elif indivs[ind].rco > w/2: #version 2
-                p = indivs[ind].rco - (math.sqrt(math.pow(indivs[ind].rco,2) - (math.pow(w,2)/4)))
-                a = w - 2*indivs[ind].ti
-            else:
-                print("Erreur -> rc0 < w/2")
+        #calcul de r1
+        rounded_n0 = round(indivs[ind].n0,3)
+        r1 = dic_indice[rounded_n0]
 
-            #calcul de r1
-            r1 = dic_indice[round(indivs[ind].n0,3)]
-            # print("r1",r1)
-            #calcul de theta
-            # print(indivs[ind].n0)
-            # print("ti",indivs[ind].ti)
-            if abs(indivs[ind].n0-1.35) < 0.001 :
-                theta = 2*math.atan(a/(2*p))
-                # print("a",a,"p",p)
+        if indivs[ind].isFit(rounded_n0, a, p, r1) :
+            if abs(rounded_n0-1.35) < 0.0005 :
+                theta = 2.0*math.atan(a/(2.0*p))
             else :
-                #print(r1)
-                tmp = (math.pow(r1,2)*a)/(2*p)
-                tmp2 = 1+math.pow(r1,2) - ((math.pow(r1,2)*math.pow(a,2))/(4*math.pow(p,2)))
-                tmp3 = (1+math.pow(r1,2))
-                # print("a",a,"p",p)
+                tmp = (math.pow(r1,2.0)*a)/(2.0*p)
+                tmp2 = 1.0+math.pow(r1,2.0) - ((math.pow(r1,2.0)*math.pow(a,2.0))/(4.0*math.pow(p,2.0)))
+                tmp3 = (1.0+math.pow(r1,2.0))
 
-                #print("tmp2",tmp2)
                 theta = tmp - math.sqrt(tmp2)/tmp3
 
-
             #calcul de la fitness pour cet individu
-            if abs(indivs[ind].n0-1.35) < 0.001:
-                #v = (0.375*p/a*math.sqrt(math.log10(0.746*a**2*math.sqrt(light))/math.log10(math.exp(1))))
-                partie1 = 0.746* math.pow(a,2) * math.sqrt(light)
-                # print("partie1", partie1)
+            if abs(rounded_n0-1.35) < 0.0005:
+                partie1 = 0.746* math.pow(a,2.0) * math.sqrt(light)
                 partie2 = math.sqrt(math.log(partie1))
                 v = 0.375 * (p / a) * partie2
             else :
-                v = 1/theta
+                v = 1.0/theta
+
+            #selection du meilleur de la generation
+            if v > vmax:
+                vmax = v
+                best = indivs[ind]
+                best_p = p
+                best_a = a
+                best_r1 = r1
+                best_theta = theta
+
+            #somme des caracteristiques pour calcul des moyennes
+            sum_rco = sum_rco + indivs[ind].rco
+            sum_ti = sum_ti + indivs[ind].ti
+            sum_at = sum_at + indivs[ind].at
+            sum_n0 = sum_n0 + indivs[ind].n0
+            sum_p = sum_p + p
+            sum_a = sum_a + a
+            sum_r1 = sum_r1 + r1
+            sum_theta = sum_theta + theta
+            sum_v = sum_v + v
+
             #sauvegarde des individus valides
             indivs[ind].fit = v
             fit_indvs.append(indivs[ind])
-            dataFile.write(str(it)+","+str(indivs[ind].rco) + "," +str(indivs[ind].ti) + "," +str(indivs[ind].at) +"," +str(indivs[ind].n0)+
-             "," +str(p)+ ","+str(a)+","+str(r1)+"," +str(theta)+","+str(v)+"\n")
 
-    #print(fit_indvs)
+    pop_size = len(fit_indvs)
+
+    #sauvegarde des caracteristiques du meilleur individu de la generation
+    dataFile.write(str(it)+",best,"+str(best.rco) + "," +str(best.ti) + "," +str(best.at) +"," +str(best.n0)+
+     "," +str(best_p)+ ","+str(best_a)+","+str(best_r1)+"," +str(best_theta)+","+str(vmax)+"\n")
+
+    #sauvegarde des caracs moyennes pour une generation
+    dataFile.write(str(it)+",mean,"+str(sum_rco/pop_size) + "," +str(sum_ti/pop_size) + "," +str(sum_at/pop_size) +"," +str(sum_n0/pop_size)+
+    "," +str(sum_p/pop_size)+ ","+str(sum_a/pop_size)+","+str(sum_r1/pop_size)+"," +str(sum_theta/pop_size)+","+str(sum_v/pop_size)+"\n")
+
     #classement des individus selon leur rang
-    if (len(fit_indvs)<2) :
+    if (len(fit_indvs)<=2) :
         print("Oops, they're gone....")
         break
     fit_indvs.sort(key=operator.attrgetter('fit'))
     fit_indvs.reverse()
-    #print(fit_indvs)
+
     #calcul de leur proba de reproduction
     len_fits = len(fit_indvs)
     coeff = (2.0/(len_fits*(len_fits-1)))
-    #print(coeff)
     p_rep = []
     for j in range(0,len_fits) :
         p_rep.append(coeff*(len_fits-j-1))
 
-    #print(p_rep)
+    # print(fit_indvs)
+    # print(p_rep)
     #génération des nouveaux individus
     nb_nv_indivs = 0
     nv_indivs = []
+    #generation des enfants issus des individus precedents
+
     while len(nv_indivs)<nb_indivs : #tant qu'il n'y a pas assez d'individus
         #selection d'une paire de parents
         par1 = select_parent(fit_indvs, p_rep)
+        # print(par1)
         par2 = None
         par2 = select_parent(fit_indvs, p_rep)
         while par1 == par2 :
             par2 = select_parent(fit_indvs, p_rep)
-            #print(par2)
-        #print("I have 2 parents")
 
         #création de 2 enfants avec cross-over
         child1 = create_child(par1, par2)
         child2 = create_child(par1, par2)
 
         #mutation chez les enfants
-        mutate(child1)
-        mutate(child2)
+        child1.mutate()
+        child2.mutate()
 
         nv_indivs.append(child1)
         nv_indivs.append(child2)
-
-        #print("parent1", par1.at, "par2", par2.at, "child1", child1.at, "child2",child2.at)
 
     indivs = nv_indivs
 
--- misc.py
+++ misc.py
@@ -16,3 +16,74 @@ def diff(v1,v2,t):
     #     return abs((v1 - v2)/v1) > t
     # else:
     return abs(v1 - v2) > t
+
+def mutateV2(child):
+    mutationChance = 0.3
+    if random.random() > mutationChance:
+        return
+    rd1 = random.randint(1, 4)
+
+    if rd1 == 1:
+        child.rco = random.uniform(w/2,10000)
+    elif rd1 == 2:
+        child.ti = random.uniform(0,w/2)
+    elif rd1  == 3:
+        child.at = random.uniform(0,math.pi/2)
+    else:
+        child.n0 = random.uniform(1.35,1.55)
+
+def mutateV3(child):
+    rd1 = random.randint(1, 2)
+    if rd1 == 1:
+        child.rco = random.uniform(w/2,10000)
+    rd2 = random.randint(1, 4)
+    if rd2 == 1:
+        child.ti = random.uniform(0,w/2)
+    rd3 = random.randint(1, 4)
+    if rd3  == 1:
+        child.at = random.uniform(0,math.pi/2)
+    rd4 = random.randint(1, 4)
+    if rd4 == 1:
+        child.n0 = random.uniform(1.35,1.55)
+
+def generate_new_indiv():
+    fit = False
+    while not fit:
+        rco = random.uniform(w/2,10000)
+        ti = random.uniform(0,w/2)
+        at = random.uniform(0,math.pi/2)
+        n0 = random.uniform(1.35,1.55)
+        ind = Individual(rco, ti, at, n0)
+
+        if abs(ind.rco-w/2.0) < tshld: #version 1
+            p = (w/2.0)*(1.0 + math.sin(ind.at))
+            a = w*math.cos(ind.at)-2.0*ind.ti
+        elif ind.rco > w/2.0: #version 2
+            p = ind.rco - (math.sqrt(math.pow(ind.rco,2.0) - (math.pow(w,2.0)/4.0)))
+            a = w - 2.0*ind.ti
+
+        #calcul de r1
+        rounded_n0 = round(ind.n0,3)
+        r1 = dic_indice[rounded_n0]
+        print("trying...", ind.isFit(rounded_n0, a, p, r1))
+        if ind.isFit(rounded_n0, a, p, r1):
+            fit = True
+    print("new individual generated")
+    return ind
+
+def mutate(child):
+    if random.random() < 0.75:
+        child.mutate_roughly()
+    else:
+        child.mutate_gently()
+
+    def mutate_roughly(self) :
+        rand = random.randint(1,4)
+        if rand == 1:
+            self.rco = random.uniform(w/2,10000)
+        elif rand == 2:
+            self.ti = random.uniform(0,w/2)
+        elif rand  == 3:
+            self.at = random.uniform(0,math.pi/2)
+        else:
+            self.n0 = random.uniform(1.35,1.55)
